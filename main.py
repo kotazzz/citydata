2c97fd723e6e7d64018d3b0411fadcaa48ea45b8
Kotaz <semechkagent@gmail.com>
2023-05-07 18:33:41 +0300

    HTML база вынесена в отдельный файл

--- main.py
+++ main.py
@@ -74,20 +74,7 @@ def main():
         # Запись всего в файл
         index = os.path.join(tmp, "index.html")
         with open(index, "w", encoding="utf-8") as file:
-            html = f"""
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-            </head>
-            <body>
-                {base}
-            </body>
-            </html>
-            """
+            html = open('base.html').read().format(base=base)
             file.write(html)
 
         # Перемещаемся в временную папку и начинаем хостить там index.html на порту PORT
